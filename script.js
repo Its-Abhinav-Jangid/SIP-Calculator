644ec4bad3bfb97873c23fd21a83ef58c81bf50f
Abhinav-Jangid <zangidnehul2020@gmail.com>
2025-01-30 13:39:18 +0530

    Added responsiveness for mobile devices

--- script.js
+++ script.js
@@ -1,132 +1,167 @@
 document.addEventListener("keydown", (e) => {
-	if (e.key === "Enter") calculate();
+  if (e.key === "Enter") calculate();
 });
+
 const sliders = document.querySelectorAll("input[type='range']");
 for (let slider of sliders) {
-	updateSlider(slider);
+  updateSlider(slider);
 }
+let myChart;
+if (window.screen.width < 600) {
+  const chartCanvas = document.getElementById("chart-mobile");
+  myChart = new Chart(chartCanvas, {
+    type: "doughnut",
+    data: {
+      labels: ["Returns", "Amount Invested"],
+      datasets: [
+        {
+          label: "",
+          data: [40, 60],
+          backgroundColor: ["hsl(233.02deg 100% 66.27%)", "orange"],
+        },
+      ],
+    },
+    options: {
+      plugins: {
+        legend: {
+          labels: {
+            color: "#666", // Text color
+            font: {
+              size: 14, // Font size
+              family: "Rubik", // Font family
+            },
+            padding: 20, // Padding around labels
+            boxWidth: 15, // Width of the color box
+            boxHeight: 10, // Height of the color box
+          },
+          position: "top", // Position of the legend (e.g., 'top', 'bottom', 'left', 'right')
+        },
+      },
+    },
+  });
+  chartCanvas.style.width = "320px";
+  chartCanvas.style.height = "320px";
+} else {
+  const chartCanvas = document.querySelector(".chart");
 
-const chartCanvas = document.querySelector(".chart");
-
-const myChart = new Chart(chartCanvas, {
-	type: "doughnut",
-	data: {
-		labels: ["Returns", "Amount Invested"],
-		datasets: [
-			{
-				label: "",
-				data: [40, 60],
-				backgroundColor: ["hsl(233.02deg 100% 66.27%)", "orange"],
-			},
-		],
-	},
-	options: {
-		plugins: {
-			legend: {
-				labels: {
-					color: "#666", // Text color
-					font: {
-						size: 14, // Font size
-						family: "Rubik", // Font family
-					},
-					padding: 20, // Padding around labels
-					boxWidth: 15, // Width of the color box
-					boxHeight: 10, // Height of the color box
-				},
-				position: "top", // Position of the legend (e.g., 'top', 'bottom', 'left', 'right')
-			},
-		},
-	},
-});
-
+  myChart = new Chart(chartCanvas, {
+    type: "doughnut",
+    data: {
+      labels: ["Returns", "Amount Invested"],
+      datasets: [
+        {
+          label: "",
+          data: [40, 60],
+          backgroundColor: ["hsl(233.02deg 100% 66.27%)", "orange"],
+        },
+      ],
+    },
+    options: {
+      plugins: {
+        legend: {
+          labels: {
+            color: "#666", // Text color
+            font: {
+              size: 14, // Font size
+              family: "Rubik", // Font family
+            },
+            padding: 20, // Padding around labels
+            boxWidth: 15, // Width of the color box
+            boxHeight: 10, // Height of the color box
+          },
+          position: "top", // Position of the legend (e.g., 'top', 'bottom', 'left', 'right')
+        },
+      },
+    },
+  });
+}
 calculate();
 function formatAmount(value) {
-	value = String(value);
-	if (value.length <= 3) return value;
-	let index = 0;
-	value =
-		value.substring(0, value.length - 3) +
-		"," +
-		value.substring(value.length - 3, value.length);
-	for (let i = value.length - 3; i >= 0; i--) {
-		index++;
-		if (index % 2 === 0 && index !== 2) {
-			value =
-				value.substring(0, i) + "," + value.substring(i, value.length);
-		}
-	}
-	if (index % 2 === 0) {
-		value = value.substring(1, value.length); // remove any leading comma
-	}
-	return value;
+  value = String(value);
+  if (value.length <= 3) return value;
+  let index = 0;
+  value =
+    value.substring(0, value.length - 3) +
+    "," +
+    value.substring(value.length - 3, value.length);
+  for (let i = value.length - 3; i >= 0; i--) {
+    index++;
+    if (index % 2 === 0 && index !== 2) {
+      value = value.substring(0, i) + "," + value.substring(i, value.length);
+    }
+  }
+  if (index % 2 === 0) {
+    value = value.substring(1, value.length); // remove any leading comma
+  }
+  return value;
 }
 
 function calculate() {
-	const amountPerMonth = parseInt(
-		document.getElementById("amount-display").value
-	);
-	const roi = parseInt(document.getElementById("roi-slider").value);
-	const duration = parseInt(document.getElementById("duration-slider").value);
-	const principleEl = document.getElementById("principle");
-	const intrestEl = document.getElementById("intrest");
-	const totalAmountEl = document.getElementById("total-amount");
-	if (
-		amountPerMonth <= 0 ||
-		roi <= 0 ||
-		duration <= 0 ||
-		!amountPerMonth ||
-		!roi ||
-		!duration || amountPerMonth > 10000000 || roi > 100 || duration > 100
-	) {
-		return;
-	}
-	const totalPrincipleCalc = amountPerMonth * duration * 12;
-	let amount = 0;
-	let intrestPerMonth = 0;
+  const amountPerMonth = parseInt(
+    document.getElementById("amount-display").value
+  );
+  const roi = parseInt(document.getElementById("roi-display").value);
+  const duration = parseInt(document.getElementById("duration-display").value);
+  const principleEl = document.getElementById("principle");
+  const intrestEl = document.getElementById("intrest");
+  const totalAmountEl = document.getElementById("total-amount");
+  if (
+    amountPerMonth <= 0 ||
+    roi <= 0 ||
+    duration <= 0 ||
+    !amountPerMonth ||
+    !roi ||
+    !duration
+  ) {
+    return;
+  }
+  const totalPrincipleCalc = amountPerMonth * duration * 12;
+  let amount = 0;
+  let intrestPerMonth = 0;
 
-	for (let i = 0; i < duration * 12; i++) {
-		amount += amountPerMonth;
-		intrestPerMonth = (roi / 100 / 12) * amount;
-		amount += intrestPerMonth;
-	}
+  for (let i = 0; i < duration * 12; i++) {
+    amount += amountPerMonth;
+    intrestPerMonth = (roi / 100 / 12) * amount;
+    amount += intrestPerMonth;
+  }
 
-	principleEl.innerHTML = `₹${formatAmount(totalPrincipleCalc)}`;
-	intrestEl.innerHTML = `₹${formatAmount(
-		Math.floor(amount - totalPrincipleCalc)
-	)}`;
-	totalAmountEl.innerHTML = `₹${formatAmount(Math.floor(amount))}`;
-	myChart.data.datasets[0].data = [
-		Math.floor(amount - totalPrincipleCalc),
-		Math.floor(totalPrincipleCalc),
-	];
-	myChart.update();
+  principleEl.innerHTML = `₹${formatAmount(totalPrincipleCalc)}`;
+  intrestEl.innerHTML = `₹${formatAmount(
+    Math.floor(amount - totalPrincipleCalc)
+  )}`;
+  totalAmountEl.innerHTML = `₹${formatAmount(Math.floor(amount))}`;
+  myChart.data.datasets[0].data = [
+    Math.floor(amount - totalPrincipleCalc),
+    Math.floor(totalPrincipleCalc),
+  ];
+  myChart.update();
 }
 function updateAmount(value) {
-	const amountLabel = document.getElementById("amount-display");
-	const amountSlider = document.getElementById("amount-slider");
-	amountLabel.value = value;
-	amountSlider.value = value;
-	updateSlider(amountSlider);
+  const amountLabel = document.getElementById("amount-display");
+  const amountSlider = document.getElementById("amount-slider");
+  amountLabel.value = value;
+  amountSlider.value = value;
+  updateSlider(amountSlider);
 }
 function updateDuration(value) {
-	const durationLabel = document.getElementById("duration-display");
-	const durationSlider = document.getElementById("duration-slider");
-	durationLabel.value = value;
-	durationSlider.value = value;
-	updateSlider(durationSlider);
+  const durationLabel = document.getElementById("duration-display");
+  const durationSlider = document.getElementById("duration-slider");
+  durationLabel.value = value;
+  durationSlider.value = value;
+  updateSlider(durationSlider);
 }
 function updateROI(value) {
-	const ROILabel = document.getElementById("roi-display");
-	const ROISlider = document.getElementById("roi-slider");
-	ROILabel.value = value;
-	ROISlider.value = value;
-	updateSlider(ROISlider);
+  const ROILabel = document.getElementById("roi-display");
+  const ROISlider = document.getElementById("roi-slider");
+  ROILabel.value = value;
+  ROISlider.value = value;
+  updateSlider(ROISlider);
 }
 function updateSlider(element) {
-	const min = element.min;
-	const max = element.max;
-	const curr = element.value;
-	const percentage = ((curr - min) / (max - min)) * 100;
+  const min = element.min;
+  const max = element.max;
+  const curr = element.value;
+  const percentage = ((curr - min) / (max - min)) * 100;
 
-	element.style.background = `linear-gradient(to right, rgb(0, 184, 82) ${percentage}%, #ddd ${percentage}%)`;
+  element.style.background = `linear-gradient(to right, rgb(0, 184, 82) ${percentage}%, #ddd ${percentage}%)`;
 }
